59856f8eea1d34b51e02232b94da6ba37fe29120
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-04 06:47:17 -0700

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -4,7 +4,7 @@ def integerFactorial(num):
     if num == 0:
         return 1
 
-    total = 1;
+    total = 1
     while num > 1:
         total *= num
         num -= 1
@@ -12,7 +12,7 @@ def integerFactorial(num):
     return total
     
 def gamma(z):
-    return math.sqrt(math.tau / z) * (((z + 1 / (12 * z + 1 / (10 * z))) / math.e) ** z);
+    return math.sqrt(math.tau / z) * (((z + 1 / (12 * z + 1 / (10 * z))) / math.e) ** z)
     
 def factorial(num):
     if int(num) == num:
