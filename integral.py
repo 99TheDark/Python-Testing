93de301b282de06c11da3a992a03f506a9070a88
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-18 21:36:11 -0700

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,10 +1,13 @@
 import math
 
+import math
+
 def integral(func, start, stop, increment = 0.0001):
     ran = [val * increment for val in range(start, int(stop / increment))] # Decimal increment
     sum = 0
     for i in ran:
-        sum += increment * func(increment + i) # Area of the rectangle under the curve. increment + i / 2 is better generally for rougher increments
+        # increment + i / 2 is better generally for rougher increments
+        sum += increment * func(i)
     return sum
 
 def circle(x):
